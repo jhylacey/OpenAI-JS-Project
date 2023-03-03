08237c1ee38a4c112b5c6eb17cd9f68c56b19cb0
jhylacey <jhylacey@proton.me>
2023-03-03 12:19:56 -0600

    Fixed Script Linking Error

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await (fetch('https://js-codex.onrender.com', {
+  const response = await (fetch('https://js-codex.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
